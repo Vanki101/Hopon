7bc9956c8f3b0d5460dc15edc329ec76153cfadd
Samiisac <samiisac93@gmail.com>
2025-11-20 02:34:38 +0100

    Fikset testet

--- backend-javalin/src/test/java/com/hopon/core/service/DepartureServiceTest.java
+++ backend-javalin/src/test/java/com/hopon/core/service/DepartureServiceTest.java
@@ -4,13 +4,15 @@ import com.hopon.adapter.entur.MockEnturClient;
 import com.hopon.core.model.Departure;
 import org.junit.jupiter.api.Test;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import com.hopon.core.service.DepartureService;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DepartureServiceTest {
     @Test
     void testEmptyDepartures() {
         MockEnturClient mockClient = new MockEnturClient();
-        DepartureService service = new DepartureService(mockClient);
+        com.hopon.core.service.DepartureService service = new DepartureService(mockClient);
 
         List<Departure> result = service.getDepartures("123");
         assertFalse(result.isEmpty(), "Departures should not be empty");
